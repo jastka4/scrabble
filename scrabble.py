dc1482b393b09569e326a5f09b6bc007a61e1965
Justyna Skalska <jastka4@gmail.com>
2019-05-18 14:30:16 +0200

    Find available words to put on the board

--- scrabble.py
+++ scrabble.py
@@ -4,6 +4,9 @@ import itertools
 import numpy
 import random
 
+# TODO - don't include already existing words in available words list (new word is shorter than the old word)
+# TODO - check if on the left side of a word is a letter, then not include chars list
+# TODO - put letters on the board
 
 def update_my_letters(my_letters, all_letters):
     for x in xrange(7 - len(my_letters)):
@@ -15,31 +18,74 @@ def is_sub_array(sub, lst):
     ln = len(sub)
     for i in range(len(lst) - ln + 1):
         if all(sub[j] == lst[i + j] for j in range(ln)):
-            return True
-    return False
-
-
-def get_permutations_of_my_letters(letters):
-    permutations = []
-    for x in itertools.permutations(letters):
-        permutations.append(x)
-    return permutations
+            return i
+    return -1
 
 
 def get_available_words(permutations, all_words):
     words = []
     for permutation in permutations:
         for word in all_words:
-            if is_sub_array(word, permutation):
-                words.append(permutation)
+            if is_sub_array(word, permutation) == 0:
+                if word not in words:
+                    words.append(word)
     return words
 
 
+def get_empty_places(board_letters):
+    empty_places = 0
+    for x in range(len(board_letters)):
+        if not board_letters[x].strip():
+            empty_places += 1
+    return empty_places
+
+
 def fill_empty_places(my_letters, board_letters):
-    return
+    possibilities = []
+    empty_places = get_empty_places(board_letters)
+    additional_letters = list(itertools.permutations(my_letters, empty_places))
+    for row in range(len(additional_letters)):
+        temp_board_letters = list(board_letters)
+        col = 0
+        for x in range(len(temp_board_letters)):
+            if not temp_board_letters[x].strip():
+                temp_board_letters[x] = additional_letters[row][col]
+                col += 1
+        possibilities.append(temp_board_letters)
+    return possibilities
 
 
-def get_permutations_of_my_lettersAndBoard(my_letters, board_letters):
+def scan_horizontally(board, my_letters, words):
+    # scanning left-right looking for neighbours top-down
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if board[x][y].strip():
+                chars = []
+                for neighbour in range(x, x + 7 + 1):
+                    if neighbour < len(board):
+                        chars.append(board[neighbour][y])
+                available_words = get_available_words(fill_empty_places(my_letters, chars), words)
+                if available_words:
+                    print chars
+                    print available_words
+
+
+def scan_vertically(board, my_letters, words):
+    # scanning top-down looking for neighbours left-right
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if board[x][y].strip():
+                chars = []
+                for neighbour in range(y, y + 7 + 1):
+                    if neighbour < len(board):
+                        chars.append(board[x][neighbour])
+                available_words = get_available_words(fill_empty_places(my_letters, chars), words)
+                if available_words:
+                    print chars
+                    print available_words
+
+
+def get_permutations_of_my_letters_and_board(my_letters, board_letters):
     return
 
 
@@ -63,39 +109,17 @@ def main():
     words = [['T', 'E', 'A'], ['H', 'U', 'G'], ['B', 'U', 'G'], ['F', 'I', 'N', 'D'], ['T', 'H', 'A', 'N', 'K'],
              ['A', 'N']]
 
-    letters = ['E'] * 12 + ['A'] * 9 + ['I'] * 9 + ['O'] * 8 + ['N'] * 6 + ['R'] * 6 + ['T'] * 6 + ['L'] * 4 + [
-        'S'] * 4 + ['U'] * 4 + ['D'] * 4 + ['G'] * 3 + ['B'] * 2 + ['C'] * 2 + ['M'] * 2 + ['P'] * 2 + ['F'] * 2 + [
-                  'H'] * 2 + ['V'] * 2 + ['W'] * 2 + ['Y'] * 2 + ['K'] + ['J'] + ['X'] + ['Q'] + ['Z']
+    letters = ['E'] * 12 + ['A'] * 9 + ['I'] * 9 + ['O'] * 8 + ['N'] * 6 + ['R'] * 6 + ['T'] * 6 + ['L'] * 4 + ['S'] \
+              * 4 + ['U'] * 4 + ['D'] * 4 + ['G'] * 3 + ['B'] * 2 + ['C'] * 2 + ['M'] * 2 + ['P'] * 2 + ['F'] \
+              * 2 + ['H'] * 2 + ['V'] * 2 + ['W'] * 2 + ['Y'] * 2 + ['K'] + ['J'] + ['X'] + ['Q'] + ['Z']
 
     my_letters = update_my_letters([], letters)
 
     # while(len(letters) > 0):
     print(numpy.matrix(board))
 
-    permutations = get_permutations_of_my_letters(my_letters)
-
-    available_words = get_available_words(permutations, words)
-
-    # scanning left-right looking for neighbours top-down
-    for x in range(len(board)):
-        for y in range(len(board[x])):
-            if board[x][y].strip():
-                chars = []
-                for neighbour in range(x, x + 7 + 1):
-                    if neighbour < len(board):
-                        chars.append(board[neighbour][y])
-                # print chars
-
-    # scanning top-down looking for neighbours left-right
-    for x in range(len(board)):
-        for y in range(len(board[x])):
-            if board[x][y].strip():
-                chars = []
-                for neighbour in range(y, y + 7 + 1):
-                    if neighbour < len(board):
-                        chars.append(board[x][neighbour])
-                # print chars
-                fill_empty_places(my_letters, chars)
+    scan_horizontally(board, my_letters, words)
+    scan_vertically(board, my_letters, words)
 
 
 if __name__ == "__main__":
